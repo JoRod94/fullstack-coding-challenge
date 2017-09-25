a4192bb9abcc43397508467ed71b4ec9e7cbf56b
JoRod94 <joao.rod94@gmail.com>
2017-09-25 16:58:43 +0100

    Added story removal database unit test

--- db_tests.py
+++ db_tests.py
@@ -23,13 +23,22 @@ def exists_translation(translation_id):
     saved_translation = translations.get(translation_id)
     return result and saved_translation
 
+def get_comment_ids(comment_id):
+    result = [comment_id]
+    saved_comment = comments.get(comment_id)
+    if 'kids' in saved_comment:
+        for kid_id in saved_comment['kids']:
+            result + get_comment_ids(kid_id)
+
+    return result
+
 class DBTestCase(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
 
     # test story insertion, its comments, and translations with a sample post https://news.ycombinator.com/item?id=397537
-    def test_stories_db(self):
+    def test_story_insertion(self):
         print("Testing story database insertion...")
 
         result = True
@@ -45,6 +54,35 @@ class DBTestCase(unittest.TestCase):
             if not result:
                 break
         result = result and exists_translation(saved_story['translation_a']) and exists_translation(saved_story['translation_b'])
+        stories.delete_one(test_id)
+        assert result
+
+    def test_story_removal(self):
+        print("Testing story database removal...")
+
+        result = True
+        test_id = 397537
+
+        sample_story = request_hn_item(test_id)
+        insert_top_story(test_id)
+        saved_story = stories.get(test_id)
+
+        comment_ids = []
+        for kid_id in saved_story['kids']:
+            comment_ids += get_comment_ids(kid_id)
+
+        translation_a_id = saved_story['translation_a']
+        translation_b_id = saved_story['translation_b']
+
+        stories.delete_one(test_id)
+
+        result = True
+        result = result and not stories.get(test_id)
+
+        for c_id in comment_ids:
+            result = result and not comments.get(c_id)
+
+        result = result and not translations.get(translation_a_id) and not translations.get(translation_b_id)
 
         assert result
 
